73c521fdb2b134b2e9f5869d4c5991d8cf99aa8e
Waldir Junior <waldirjunior.gpn@gmail.com>
2023-04-22 18:30:50 -0300

    Fazendo tratamento no método calculaGratificacao

--- MundoDoEstudante/src/br/com/MundoDoEstudante/classes/Atendente.java
+++ MundoDoEstudante/src/br/com/MundoDoEstudante/classes/Atendente.java
@@ -17,9 +17,9 @@ public class Atendente extends Funcionario implements Gratificacao {
 	private double gratificacaoTotal;
 	static private List<Atendente> lista = new ArrayList<>();
 	static private Map<String, List<Double>> lojas = new HashMap<>();
-	
+
 	public static void calculadorGratificacao(String loja) {
-		
+		try {
 		List<Double> percentual = lojas.get(loja);
 		initLojas();
 		
@@ -32,30 +32,33 @@ public class Atendente extends Funcionario implements Gratificacao {
 			
 			lista.subList(3, lista.size()).forEach(atendentes -> 
 				atendentes.setGratificacaoSemana(atendentes.getVendasSemana() * percentual.get(3)));
+		} else {
+			throw new Excepcion("É necessário que informe o nome da Loja, Mundo, Sonho, Vovo ou Lapis, para poder usar a regra de gratificação correta");
 		}
+	} catch(Exception e) {
+		e.printStackTrace();
 	}
-	
+
 	public static void ordenarVendaTotal() {
 		lista.sort(Comparator.comparing(Atendente::getVendasTotal));
 		Collections.reverse(lista);
 	}
-	
+
 	public static void imprimeResultado() {
 		lista.forEach(System.out::println);
 	}
-	
+
 	private static void initLojas() {
 		lojas.put("Mundo", Arrays.asList(0.01, 0.008, 0.006, 0.005));
 		lojas.put("Sonho", Arrays.asList(0.01, 0.008, 0.006, 0.005));
 		lojas.put("Casa", Arrays.asList(0.009, 0.007, 0.005, 0.004));
 		lojas.put("Lapis", Arrays.asList(0.01, 0.008, 0.006, 0.005));
 	}
-	
+
 	public void cadastra(Atendente atende) {
 		lista.add(atende);
 	}
-	
-	
+
 	public Atendente(String nome) {
 		super(nome);
 		lista.add(this);
@@ -72,7 +75,7 @@ public class Atendente extends Funcionario implements Gratificacao {
 		this.gratificacaoTotal += valor;
 
 	}
-	
+
 	public double getGratificacaoTotal() {
 		return this.gratificacaoTotal;
 	}
@@ -109,8 +112,8 @@ public class Atendente extends Funcionario implements Gratificacao {
 	@Override
 	public String toString() {
 
-		String novaString = String.format(new Locale("pt", "BR"), "Funcionario: %s%nVendas: %.2f%nGratificacao: %.2f%n"
-				, this.getNome(), this.getVendasTotal(), this.getGratificacaoTotal());
+		String novaString = String.format(new Locale("pt", "BR"), "Funcionario: %s%nVendas: %.2f%nGratificacao: %.2f%n",
+				this.getNome(), this.getVendasTotal(), this.getGratificacaoTotal());
 		return novaString;
 
 	}

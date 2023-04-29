d3067752868ec68e7ea862dea0560e03625393f2
Waldir Junior <waldirjunior.gpn@gmail.com>
2023-04-29 14:57:57 -0300

    Alterando percentual de gratificacao

--- MundoDoEstudante/src/br/com/MundoDoEstudante/classes/calculadoraGratificacao/VovoCalculadoraGratificacao.java
+++ MundoDoEstudante/src/br/com/MundoDoEstudante/classes/calculadoraGratificacao/VovoCalculadoraGratificacao.java
@@ -7,10 +7,10 @@ import br.com.MundoDoEstudante.classes.Atendente;
 
 public class VovoCalculadoraGratificacao extends CalculadorGratificacao{
 
-	private static final Double PRIMEIRO_COLOCADO = 0.01;
-	private static final Double SEGUNDO_COLOCADO = 0.008;
-	private static final Double TERCEIRO_COLOCADO = 0.006;
-	private static final Double DEMAIS_COLOCADOS = 0.005;
+	private static final Double PRIMEIRO_COLOCADO = 0.009;
+	private static final Double SEGUNDO_COLOCADO = 0.007;
+	private static final Double TERCEIRO_COLOCADO = 0.005;
+	private static final Double DEMAIS_COLOCADOS = 0.004;
 	private List<Atendente> gratificacoes = new ArrayList<>();
 	
 	public VovoCalculadoraGratificacao(List<Atendente> lista) {
